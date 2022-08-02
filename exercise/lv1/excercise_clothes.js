6233f72867ef432c2b9f94251ce077fb137fe78e
eotkds <eotkds@gmail.com>
2022-08-02 12:05:55 +0900

    220802 Refactoring excercise_clothes

--- exercise/lv1/excercise_clothes.js
+++ exercise/lv1/excercise_clothes.js
@@ -125,7 +125,85 @@ function solution(n, lost, reserve) {
     }, 0);
     return score;
   });
+
   let reserved_person = Math.max(...result);
 
   return n - lost_arr.length + reserved_person;
 }
+
+//220802 refactoring : 2, 10 런타임 에러
+function solution(n, lost, reserve) {
+  let answer = [];
+  //1. 잃어버렸는데 여벌이 있는 경우 제외
+  let lost_arr = [];
+  let reserve_arr = [];
+  for (let i = 0; i < lost.length; i++) {
+    if (!reserve.includes(lost[i])) lost_arr.push(lost[i]);
+  }
+  for (let i = 0; i < reserve.length; i++) {
+    if (!lost.includes(reserve[i])) reserve_arr.push(reserve[i]);
+  }
+  // console.log(lost_arr, reserve_arr);
+  if (lost_arr.length === 0 || reserve_arr.length === 0)
+    return n - lost_arr.length;
+  //케이스 만들기
+  for (let i = 0; i < 2 ** lost_arr.length; i++) {
+    let score = new Set();
+    let s = i.toString(2).padStart(lost_arr.length, "0");
+
+    for (let j = 0; j < s.length; j++) {
+      if (s[j] == 0) {
+        if (reserve_arr.includes(lost_arr[j] - 1)) score.add(lost_arr[j] - 1);
+      }
+      if (s[j] == 1) {
+        if (reserve_arr.includes(lost_arr[j] + 1)) score.add(lost_arr[j] + 1);
+      }
+    }
+    answer.push([...score].length);
+  }
+
+  return n - lost_arr.length + Math.max(...answer);
+}
+
+//정렬 후 refactoring : 케이스5, 12 실패 -> 질문하기 참고하여 성공 let lost = [ 1, 2], let reserve = [2, 3]
+function solution(n, lost, reserve) {
+  let answer = [];
+  //1. 잃어버렸는데 여벌이 있는 경우 제외
+  let lost_arr = [];
+  let reserve_arr = [];
+  for (let i = 0; i < lost.length; i++) {
+    if (!reserve.includes(lost[i])) lost_arr.push(lost[i]);
+  }
+  for (let i = 0; i < reserve.length; i++) {
+    if (!lost.includes(reserve[i])) reserve_arr.push(reserve[i]);
+  }
+  //2. 정렬
+  lost_arr.sort((a, b) => a - b);
+  reserve_arr.sort((a, b) => a - b);
+
+  //3. 분배
+  for (let i = 0; i < lost_arr.length; i++) {
+    let v1 = reserve_arr.indexOf(lost_arr[i] - 1),
+      v2 = reserve_arr.indexOf(lost_arr[i] + 1),
+      v3 = reserve_arr.indexOf(lost_arr[i]);
+    if (v3 !== -1) {
+      answer.push(reserve_arr[v3]);
+      continue;
+    }
+
+    if (v1 !== -1) {
+      if (!answer.includes(reserve_arr[v1])) {
+        answer.push(reserve_arr[v1]);
+        continue;
+      }
+    }
+
+    if (v2 !== -1) {
+      if (!answer.includes(reserve_arr[v2])) {
+        answer.push(reserve_arr[v2]);
+      }
+    }
+  }
+  return n - lost_arr.length + answer.length;
+}
+//reference refactoring
