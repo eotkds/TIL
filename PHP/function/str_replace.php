68a920ff49e05e3d7529573e40fc782b74997a21
eotkds <eotkds@gmail.com>
2022-11-04 21:46:43 +0900

    221104 studying javascirpt : jquery_ajax, PHP fucntion : str_replace.php

--- PHP/function/str_replace.php
+++ PHP/function/str_replace.php
@@ -1,8 +1,17 @@
 <?php 
-$str = "<p>abc.mp4</p><div>efg mp4</div> .mp4";
+// $str = "<p>abc.mp4</p><div>efg mp4</div> .mp4";
 // $content = str_replace("/mp4/", "/", $str); // 정규 표현식으로는 적용x
 // echo $content."<br />";
-$content = str_replace(".mp4", ".gif", $str);
-echo $content."<br />";
+// $content = str_replace(".mp4", ".gif", $str);
+// echo $content."<br />";
+
+$str = " 2,000원  ";
+echo $str."<br/>";
+$str =  trim($str);
+echo $str."<br/>";
+$str = str_replace("원", "", $str);
+echo $str."<br/>";
+
+
 
 ?>
