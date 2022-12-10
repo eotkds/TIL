c06c1b683173e6ccdabc8a71e60c65e1730f774a
eotkds <eotkds@gmail.com>
2022-12-10 11:11:35 +0900

    221210 studying vanilla_JS : nomad course by 4-2

--- HTML/vanilla_JS/chrome_app_nomad/4-0_input_value.js
+++ HTML/vanilla_JS/chrome_app_nomad/4-0_input_value.js
@@ -4,9 +4,9 @@ const loginForm = document.querySelector('#login-form');
 //아래처럼 가능
 const loginInput = loginForm.querySelector("#login-form input");
 const loginButton = loginForm.querySelector("#login-form button");
-console.log(loginForm);
-console.log(loginInput);
-console.log(loginButton);
+// console.log(loginForm);
+// console.log(loginInput);
+// console.log(loginButton);
 
 function onLoginBtnClick(){
     const value = loginInput.value;
@@ -16,10 +16,21 @@ function onLoginBtnClick(){
         alert("Your name is too long");
     }
 }
+function onLoginSubmit(e){
+    console.log(e);
+    e.preventDefault();
+    // if(value === ""){
+    //     alert("Please write your name");
+    // }else if(value.length > 15){
+    //     alert("Your name is too long");
+    // }
+}
 /**
  * form, submit 방식을 사용하면 
  * addEventListener 방식을 사용할 필요가 없다.
  * but
  * form submit 을 하면 페이지가 refresh 된다.
  */
-loginButton.addEventListener("click", onLoginBtnClick);
+
+// loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
