c589a531c57192481e2fc2acf6a9ea9f2cbb8d74
eotkds <eotkds@gmail.com>
2022-12-11 19:12:09 +0900

    221211 studying vanilla_JS : nomad course by 4-6

--- HTML/vanilla_JS/chrome_app_nomad/4-5-saving_username.js
+++ HTML/vanilla_JS/chrome_app_nomad/4-5-saving_username.js
@@ -5,16 +5,30 @@ const loginInput = loginForm.querySelector("#login-form input");
 const loginButton = loginForm.querySelector("#login-form button");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 
 function onLoginSubmit(e){
     e.preventDefault();
     console.log(e);
     const username = loginInput.value;
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello + ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreetings(username);
 }
 
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(username);
+}   
+
 loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+console.log(savedUsername);
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    paintGreetings(savedUsername)
+}
