080d54033c383139378c22fba79e270bdb471cfa
eotkds <eotkds@gmail.com>
2022-12-16 22:52:50 +0900

    studying vanilla_JS : nomad course by 5.3

--- HTML/vanilla_JS/chrome_app_nomad/course_5/js/clock.js
+++ HTML/vanilla_JS/chrome_app_nomad/course_5/js/clock.js
@@ -2,7 +2,12 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    /* String으로 감싸 된다. */
+    const hour = date.getHours() + "";
+    const minute = date.getMinutes() + "";
+    const second = date.getSeconds() + "";
+
+    clock.innerText = `${hour.padStart(2,"0")}:${minute.padStart(2,"0")}:${second.padStart(2,"0")}`;
 }
 getClock();
 setInterval(getClock, 1000);
