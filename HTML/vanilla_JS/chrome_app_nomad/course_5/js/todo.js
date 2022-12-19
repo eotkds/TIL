d27c43421bf94a09ccb98f91d1e8d9635ccfcbf2
eotkds <eotkds@gmail.com>
2022-12-19 23:11:18 +0900

    studying vanilla_JS : nomad course by 7.2

--- HTML/vanilla_JS/chrome_app_nomad/course_5/js/todo.js
+++ HTML/vanilla_JS/chrome_app_nomad/course_5/js/todo.js
@@ -2,10 +2,26 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input"); // document 뿐만 아니라 tag로도 검색 가능.
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(e){
+    /*
+    console.log(e);
+    console.log(e.target);
+    console.log(this.parentNode);
+    this.parentNode.remove();
+    */
+   const li = e.target.parentNode;
+   li.remove();
+
+}
+
 function paintToDo(newTodo){
     const liTodo = document.createElement("li");
     const spanTodo = document.createElement("span");
+    const button =document.createElement("button");
+    button.innerText ="❌";
+    button.addEventListener("click",deleteToDo)
     liTodo.appendChild(spanTodo);
+    liTodo.appendChild(button);
     spanTodo.innerText = newTodo;
     toDoList.appendChild(liTodo);
 }
