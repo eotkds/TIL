08572f90a1fbab395f78254545d0af141204ddfe
eotkds <eotkds@gmail.com>
2022-12-28 00:03:49 +0900

    vanilla_JS : nomad course by 8.1

--- HTML/vanilla_JS/chrome_app_nomad/course_5/js/weather.js
+++ HTML/vanilla_JS/chrome_app_nomad/course_5/js/weather.js
@@ -1,7 +1,15 @@
+const API_KEY = "133850ccc8db1c16eea88494908c8f25";
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log(lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric&lang=kr`
+    fetch(url).then(res => res.json()).then(data =>{
+        const weather = document.querySelector('#weather span:first-child');
+        const city = document.querySelector('#weather span:last-child');
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`
+    });
 }
 
 function onGeoError(){
