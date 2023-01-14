06bcc4ba5190111fc13dacfeb12476b05ca332ce
eotkds <eotkds@gmail.com>
2023-01-14 23:16:54 +0900

    vanilla_JS : swiper excercise

--- HTML/vanilla_JS/swiper/swiper_excercise.js
+++ HTML/vanilla_JS/swiper/swiper_excercise.js
@@ -1,7 +1,16 @@
-const swiper_box = document.getElementById('swiper_box');
+const swiper_list = document.querySelector('#swiper_box ul');
+const swiper_lists = document.querySelectorAll('#swiper_box .page')
+const lastIndex =  swiper_lists.length - 1;
 console.log(swiper_box);
 console.log(swiper_box.clientWidth);
+console.log(swiper_lists[lastIndex]);
 
 
-// (function(){
-// })
+
+const cloneElement =()=> {
+    swiper_list.insertBefore(swiper_lists[lastIndex].cloneNode(true), swiper_list.children[0]);
+    swiper_list.appendChild(swiper_lists[0].cloneNode(true));
+};
+
+
+cloneElement();
