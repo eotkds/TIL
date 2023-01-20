b9f9cd596d036fea672d560b27c7881d41c5a7c0
eotkds <eotkds@gmail.com>
2023-01-20 15:40:14 +0900

    function test  : strpos

--- PHP/function/strpos.php
+++ PHP/function/strpos.php
@@ -1,6 +1,11 @@
 <?php 
 echo "eotkds<br />";
-$str = "eotkds.com";
-var_dump(strpos(".".$str, "eotkds")); 
+$str = "[] 영광법성포 장줄 2호";
+if(strpos($str, "백화점프리미엄") || strpos($str, "선물세트")){
+
+    var_export("test"); 
+}else{
+    var_export("asdfa");
+}
 
 ?>
