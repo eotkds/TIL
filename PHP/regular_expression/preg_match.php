d970d169ec2ba731494a554a0f484012302996cd
eotkds <eotkds@gmail.com>
2023-02-09 11:05:44 +0900

    PHP : function excercise preg_match

--- PHP/regular_expression/preg_match.php
+++ PHP/regular_expression/preg_match.php
@@ -1,11 +1,16 @@
 <?php 
 $str = '<p><br></p><p><img src="/data/daumeditor10/221018/35567916660497720.gif" class="txc-image" width="240" style="""" /></p><p><br></p>';
 preg_match_all("/<img([^>]*)>/i",$str, $matches);
-var_export($matches[1]);
+// var_export($matches[1]);
 
 
-if(true){
-	var_export('test');
-	// var_export($board_skin_path);
-}
+// if(true){
+// 	var_export('test');
+// 	// var_export($board_skin_path);
+// }
+
+
+$str = '자유게시판';
+$result = preg_match("/자유게시판입니다/", "$str");
+var_export($result);
 ?>
