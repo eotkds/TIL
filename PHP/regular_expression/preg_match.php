ad7d62866dd408d84103a43b94697d135a4b2a0a
eotkds <eotkds@gmail.com>
2023-02-09 11:38:07 +0900

    PHP : function excercise preg_match

--- PHP/regular_expression/preg_match.php
+++ PHP/regular_expression/preg_match.php
@@ -2,14 +2,13 @@
 $str = '<p><br></p><p><img src="/data/daumeditor10/221018/35567916660497720.gif" class="txc-image" width="240" style="""" /></p><p><br></p>';
 preg_match_all("/<img([^>]*)>/i",$str, $matches);
 // var_export($matches[1]);
-
-
 // if(true){
 // 	var_export('test');
 // 	// var_export($board_skin_path);
 // }
 
 
+/* 붙어있는 글자가 부분 글자를 찾는지? */
 $str = '자유게시판';
 $result = preg_match("/자유게시판입니다/", "$str");
 var_export($result);
