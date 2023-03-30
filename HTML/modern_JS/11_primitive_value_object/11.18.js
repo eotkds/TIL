b1505ad9388bccedabd6e706ee2349c60199c705
eotkds <eotkds@gmail.com>
2023-03-30 23:19:20 +0900

    modern_JS 11 Ending primitive value object

---
+++ HTML/modern_JS/11_primitive_value_object/11.18.js
@@ -0,0 +1,11 @@
+var person1 = {
+    name: 'Nicholas'
+};
+
+var person2 = {
+    name: 'Nicholas'
+};
+
+console.log(person1 === person2); // false
+console.log(person1.name === person2.name); // true
+
